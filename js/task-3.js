2a4f82eb3f708e23b4f3323af0d4cd5ef1ee7a4d
olesyamaniuk <maniukolesya222@gmail.com>
2024-02-12 18:19:22 +0200

    1

---
+++ js/task-3.js
@@ -0,0 +1,36 @@
+class StringBuilder {
+    initialValue;
+
+    constructor(initialValue) {
+        this.initialValue = initialValue;
+    }
+
+    getValue() {
+        return this.initialValue;
+    }
+
+    padEnd(str) {
+        this.initialValue += str;
+    }
+
+    padStart(str) {
+        this.initialValue = str + this.initialValue;
+    }
+
+    padBoth(str) {
+        this.padStart(str);
+        this.padEnd(str);
+    }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
